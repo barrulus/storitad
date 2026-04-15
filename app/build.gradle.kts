cfad4aab460334e593c0a8fa9b9590914d7aedfb
Bazza <bazza@storitad.local>
2026-04-15 14:13:27 +0100

    ci: signed APK release on tag push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "uk.storitad.capture"
         minSdk = 34
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.3.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk { abiFilters += "arm64-v8a" }
         externalNativeBuild {
