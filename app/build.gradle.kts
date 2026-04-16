99ab20bedf901451d8960640411071b66a90c655
Bazza <bazza@storitad.local>
2026-04-16 21:36:02 +0100

    release v0.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "uk.storitad.capture"
         minSdk = 34
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.3.2"
+        versionCode = 6
+        versionName = "0.3.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk { abiFilters += "arm64-v8a" }
         externalNativeBuild {
